9c1fc87a9fae9786126130eb425eaac6d5b7af4e
Vadim Bakaev <bakaev.vad@gmail.com>
2020-06-07 14:37:08 +0200

    Add leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React, { FC } from "react";
-import { Button } from "antd";
 import "./App.less";
+import { Layout, Menu } from "antd";
+import MapContent from "./components/MapContent";
+
+const { Header, Content, Footer } = Layout;
 
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">Companies</Menu.Item>
+        </Menu>
+      </Header>
+      <Content className="main-content">
+        <MapContent></MapContent>
+      </Content>
+      <Footer className="footer">flatmap.io 2020</Footer>
+    </Layout>
   </div>
 );
 
---
+++ src/components/MapContent.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import { LatLngTuple } from "leaflet";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import "./MapContent.less";
+
+const position: LatLngTuple = [5, 35];
+
+const MapContent: FC = () => {
+  return (
+    <div className="map">
+      <Map center={position} zoom={2}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup.
+            <br />
+            Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    </div>
+  );
+};
+
+export default MapContent;
